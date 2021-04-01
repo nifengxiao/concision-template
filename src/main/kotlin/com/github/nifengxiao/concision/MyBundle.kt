b4c3caf03a4c4805f8da470ceb350e30bc1f54c2
GitHub Action <action@github.com>
2021-04-01 06:59:42 +0000

    Template cleanup

--- src/main/kotlin/com/github/nifengxiao/concision/MyBundle.kt
+++ src/main/kotlin/com/github/nifengxiao/concision/MyBundle.kt
@@ -1,4 +1,4 @@
-package org.jetbrains.plugins.template
+package com.github.nifengxiao.concision
 
 import com.intellij.AbstractBundle
 import org.jetbrains.annotations.NonNls
--- src/main/kotlin/com/github/nifengxiao/concision/services/MyApplicationService.kt
+++ src/main/kotlin/com/github/nifengxiao/concision/services/MyApplicationService.kt
@@ -1,6 +1,6 @@
-package org.jetbrains.plugins.template.services
+package com.github.nifengxiao.concision.services
 
-import org.jetbrains.plugins.template.MyBundle
+import com.github.nifengxiao.concision.MyBundle
 
 class MyApplicationService {
 
